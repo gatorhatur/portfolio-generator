86fe781ae2e9021656d50490d9c862d8e9032d1f
Brandon Davis <brandon.davis@conehealth.com>
2022-07-22 11:36:15 -0400

    created generate function and .js files for import

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-
-
-
-const printProfileData = profileDataArr => profileDataArr.forEach(profileItem => console.log(profileItem));
-        
-
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
 
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portofolio complete! Check out index.html to see the output');
+});
