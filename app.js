de577bded2b0d60bf13a20e522d69fbc0ee9abbf
Brandon Davis <brandon.davis@conehealth.com>
2022-07-22 13:53:52 -0400

    implementing inquirer

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
 
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+//     console.log('Portofolio complete! Check out index.html to see the output');
+// });
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            }
+        ]);  
+}
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portofolio complete! Check out index.html to see the output');
-});
+promptUser().then(answers => console.log(answers));
